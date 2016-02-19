1a9d89028ce3ce750034baa191e88308fa6ca06c
WesleyGuo <wesleyyguo@gmail.com>
2016-02-18 23:39:08 -0800

    Updated high level controller pd.py - Scout added AR tag distance and orientation checking functionality

--- project1/src/lab1/src/pd.py
+++ project1/src/lab1/src/pd.py
@@ -14,6 +14,21 @@ from geometry_msgs.msg import Transform, Vector3, Twist
 listener = None
 zumy_stop = (Vector3(0,0,0),Vector3(0,0,0))
 
+# Check if Zumy is close enought to target position and rotation
+def checkTThresh(trans, rot):
+    tthresh = .1
+    for i in range(0,2)
+        if trans(i)>ttrhesh
+            return False
+    return True
+
+def checkRThresh(trans, rot):
+    rthresh = .1
+    for i in range(0,2)
+        if trans(i)>rtrhesh
+            return False
+    return True
+
 # Tell the zumy what to do
 def command(ar_tags):
     listener = tf.TransformListener()
@@ -21,49 +36,51 @@ def command(ar_tags):
     zumyctrl_enable = rospy.Publisher("/zumy_ctrl/enable", Bool, queue_size=10)
     zumyctrl_setpoint = rospy.Publisher("/zumy_ctrl/setpoint", Float64, queue_size=10)
     r = rospy.Rate(10)
+    curr = 0
+    target = 1
 
+    while (target<len(ar_tags)):
+        # FACE THE ARTAG
+        while not rospy.is_shutdown():
+            try:
+                trans,rot = listener.lookupTransform(ar_tags['ar'+str(target)], ar_tags['ar'+str(curr)], rospy.Time(0))
+            except:
+                continue
+              
+            if (checkRThresh(trans,rot)):
+                zumy_vel.publish(zumy_stop)
+            else:
+                zumy_vel.publish(Vector3(0,0,0),Vector3(0,0,.2)) #slowly turn to face the goal AR tag
+                r.sleep()
 
-    # FACE THE ARTAG
-    while not rospy.is_shutdown():
-        try:
-            trans,rot = listener.lookupTransform(ar_tags['ar1'], ar_tags['arZ'], rospy.Time(0))
-        except:
-            continue
-        if (THRESHOLD_FROM_TAG):
-            zumy_vel.publish(zumy_stop)
-        zumy_vel.publish(Vector3(0,0,0),Vector3(0,0,.2)) #slowly turn to face the goal AR tag
-        r.sleep()
+        # GO TO AR TAG USING PID
+        zumyctrl_setpoint.publish(0.5)
+        zumyctrl_enable.publish(True)
+        while not rospy.is_shutdown():
+            #thresh = 
 
-    # GO TO AR TAG USING PID
-    zumyctrl_setpoint.publish(0.5)
-    zumyctrl_enable.publish(True)
-    while not rospy.is_shutdown():
-        thresh = 
+            if (checkTThresh(trans,rot)):
+                zumyctrl_enable.publish(False)
+                zumy_vel.publish(zumy_stop)
+            else:
+                zumy_vel.publish(Vector3(-0.8*trans[1],0,0),Vector3(0,0,0)) #slowly go to the goal tag
 
-        if (THRESHOLD_FROM_TAG):
-            zumyctrl_enable.publish(False)
-            zumy_vel.publish(zumy_stop)
-
-
-        # zumy_vel.publish(Vector3(-0.8*trans[1],0,0),Vector3(0,0,0)) #slowly go to the goal tag
+    target+=1
 
     # stop the zumy
     zumy_vel.publish(zumy_stop)
-
-
 
 def main():
     rospy.init_node('zumy_ctrl')
     # THIS IS ONLY FROM POINT A -> B IN STRAIGHT LINE
     # DOES NOT DO THE TURNING STUFF!!
-    if len(sys.argv) < 3:
-        print "Use: pd.py [AR tag number for Zumy][AR tag number for goal]"
-        sys.exit()
+    # if len(sys.argv) < 3:
+    #     print "Use: pd.py [AR tag number for Zumy][AR tag number for goal]"
+    #     sys.exit()
     ar_tags = {}
-    ar_tags['ar1'] = 'ar_marker_' + sys.argv[2]
-    ar_tags['arZ'] = 'ar_marker_' + sys.argv[1]
-
-    # Tell the zumy to move
+    for i range(0,len(sys.argv)-1)
+        ar_tags['ar'+str(i)] = 'ar_marker_' + sys.argv[i]
+        # Tell the zumy to move
     command(ar_tags)
     rospy.spin()
 
