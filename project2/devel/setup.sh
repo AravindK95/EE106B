7af4607a12b9c451df3e01432a50a18f90819b6d
AravindK95 <aravind.kumaraguru@gmail.com>
2016-02-28 15:22:51 -0800

    Configured reflex_sf code for ROS - Added helper node reflex_helper.py

--- project2/devel/setup.sh
+++ project2/devel/setup.sh
@@ -8,7 +8,7 @@
 
 # since this file is sourced either use the provided _CATKIN_SETUP_DIR
 # or fall back to the destination set at configure time
-: ${_CATKIN_SETUP_DIR:=/home/ee106/ee106b/project2/devel}
+: ${_CATKIN_SETUP_DIR:=/home/cc/ee106b/sp16/class/ee106b-am/ros_workspaces/project2/devel}
 _SETUP_UTIL="$_CATKIN_SETUP_DIR/_setup_util.py"
 unset _CATKIN_SETUP_DIR
 
---
+++ project2/src/reflex_ctrl/src/reflex_ctrl.py
@@ -0,0 +1,32 @@
+import sys
+import rospy
+import roslib
+from reflex_sf_msgs.msg import SFPose
+
+
+presets = {'o':(0.0, 0.0, 0.0),
+           'c':(3.5, 3.5, 3.5)}
+
+def main():
+    rospy.init_node("reflex_ctrl")
+    pub = rospy.Publisher('/reflex_sf/command', SFPose, queue_size=10)
+
+    while not rospy.is_shutdown():
+        cmd = raw_input("reflex pose vals/preset >> ")
+        cmd = cmd.split()
+
+        if cmd[0] in presets:
+            p = presets[cmd[0]]
+            pub.publish(p[0], p[1], p[2], 0.0)
+            continue
+
+        try:
+            cmd = [float(x) for x in cmd]
+        except ValueError:
+            print("Bad command!")
+            continue
+        pub.publish(cmd[0], cmd[1], cmd[2], 0.0)
+
+
+if __name__ == '__main__':
+    main()
