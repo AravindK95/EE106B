af49a1eaadd6870a8cae1a084ad153582c9fa14c
AravindK95 <aravind.kumaraguru@gmail.com>
2016-05-07 09:41:13 -0700

    Added helper method grasp_publisher.py to pull data from sorted.csv and publish to tf - removed grasp_ctrl.py from grasper_ctrl

--- project4/src/grasper_ctrl/src/grasp_publisher.py
+++ project4/src/grasper_ctrl/src/grasp_publisher.py
@@ -9,13 +9,11 @@ from geometry_msgs.msg import Transform, Pose, Vector3, Quaternion, Point
 from lab3.msg import FrameCall
 
 PROJECT_PATH = rospkg.RosPack().get_path('grasper_plan')
-# sys.path.append(PROJECT_PATH+'/src/lab3')
-# sys.path.append(PROJECT_PATH+'/src/extra')
-# SPRAY_BOTTLE_MESH_FILENAME = PROJECT_PATH+'/data/spray.obj'
+GRASP_DB_FILENAME = PROJECT_PATH+'/data/sorted.csv'
+sys.path.append(PROJECT_PATH+'/src')
 
 # import obj_file
-# import transformations
-# from lab3_starter import contacts_to_baxter_hand_pose
+import transformations
 
 BASE = 'base'
 OBJ_BASE = 'graspable_object'
@@ -27,18 +25,18 @@ def publish_frame_group(trans, rot, name, base, to_add):
                    base, 
                    to_add)
     #One of these is the correct direction to off set grap pos by.
-    pre_trans = Vector3(trans[0] - 0.2, trans[1], trans[2])
-    pre_rot = Quaternion(rot[0], rot[1], rot[2], rot[3])
+    # pre_trans = Vector3(trans[0] - 0.2, trans[1], trans[2])
+    # pre_rot = Quaternion(rot[0], rot[1], rot[2], rot[3])
 
     #One of these is the correct direction to lift it straight up. Probably z.
-    post_trans = Vector3(trans[0], trans[1], trans[2] + 0.3)
-    post_rot = Quaternion(rot[0], rot[1], rot[2], rot[3])
+    # post_trans = Vector3(trans[0], trans[1], trans[2] + 0.3)
+    # post_rot = Quaternion(rot[0], rot[1], rot[2], rot[3])
     #We want to the post orientation to be the same as the initial orientation during grasp
     #so we do not need to change orientation of end effector.
 
     #Publish the pre and post trans
-    tf_pub.publish(Transform(pre_trans, pre_rot), 'pre'+name, base, to_add)
-    tf_pub.publish(Transform(post_trans, post_rot), 'post'+name, base, to_add)
+    # tf_pub.publish(Transform(pre_trans, pre_rot), 'pre'+name, base, to_add)
+    # tf_pub.publish(Transform(post_trans, post_rot), 'post'+name, base, to_add)
 
 def addframe(trans, rot, name, base):
     publish_frame_group(trans, rot, name, base, True)
@@ -70,11 +68,21 @@ if __name__ == '__main__':
     # triangles = mesh.triangles
     # normals = mesh.normals
 
-    rospy.init_node('grasp_ctrl')
+    fdata = open(GRASP_DB_FILENAME, 'r')
+    data = fdata.read()
+    fdata.close()
+
+    data = data.split('\n')[1:-1]
+    data = [d.split(';')[:-1] for d in data]
+
+    grasps = [(np.array(eval(grasp_pair[0])).reshape((4,4)),
+               np.array(eval(grasp_pair[1])).reshape((4,4))) for grasp_pair in data]
+
+    rospy.init_node('grasp_publisher')
 
     tf_pub = rospy.Publisher('grasper_ctrl/tf', FrameCall, queue_size=3)
-    moveit_pub = rospy.Publisher('new_position', Pose, queue_size=3)
-    claw_pub = rospy.Publisher('gripper_control', Bool, queue_size=3)
+    # moveit_pub = rospy.Publisher('new_position', Pose, queue_size=3)
+    # claw_pub = rospy.Publisher('gripper_/control', Bool, queue_size=3)
 
     tf_listener = tf.TransformListener()
 
@@ -108,55 +116,69 @@ if __name__ == '__main__':
             name = inval[1]
             rmframe(name)
 
-        elif cmd == 'moveto':
-            # command moveit
-            """Example input: 
-               $ cmd >> moveto child
-            """
-            name = inval[1]
-            moveto(name)
-
-        elif cmd == 'setclaw':
-            # command the end effector
-            """Example input: 
-               $ cmd >> setclaw True 
-            """
-            claw_bool = eval(inval[1])
-            setclaw(claw_bool)
-
-        elif cmd == 'makepose':
-            # turn two force closure vertices into a tf frame
+        elif cmd == 'pubgrasps':
+            # publish a pair of grasps from datafile, specify by index
             """Example input:
-               $ cmd >> makepose name 2473 2035
+               $ cmd >> pubgrasps 3 child base
             """
-            name = inval[1]
-            idx1 = int(inval[2])
-            idx2 = int(inval[3])
-            makepose(name, idx1, idx2)
+            idx = eval(inval[1])
+            q0 = transformations.quaternion_from_matrix(grasps[idx][0])
+            t0 = transformations.translation_from_matrix(grasps[idx][0])
+            q1 = transformations.quaternion_from_matrix(grasps[idx][1])
+            t1 = transformations.translation_from_matrix(grasps[idx][1])
 
-        elif cmd == 'test':
-            # runs repeated tests of a single grasp
-            """Example input:
-                $ cmd >> test name
-            """
-            name = inval[1]
-            while not rospy.is_shutdown():
-                if raw_input("Test again? [y/n] >> ") == 'n':
-                    break
+            addframe(t0, q0, inval[2]+'1', inval[3])
+            addframe(t0, q0, inval[2]+'2', inval[3])
 
-                moveto('pre'+name)
-                rospy.sleep(2)
-                moveto(name)
-                rospy.sleep(2)
-                setclaw(True)
-                rospy.sleep(2)
-                moveto('post'+name)
-                rospy.sleep(4)
-                moveto(name)
-                rospy.sleep(2)
-                setclaw(False)
-                rospy.sleep(2)
-                moveto('pre'+name)
+        # elif cmd == 'moveto':
+        #     # command moveit
+        #     """Example input: 
+        #        $ cmd >> moveto child
+        #     """
+        #     name = inval[1]
+        #     moveto(name)
+
+        # elif cmd == 'setclaw':
+        #     # command the end effector
+        #     """Example input: 
+        #        $ cmd >> setclaw True 
+        #     """
+        #     claw_bool = eval(inval[1])
+        #     setclaw(claw_bool)
+
+        # elif cmd == 'makepose':
+        #     # turn two force closure vertices into a tf frame
+        #     """Example input:
+        #        $ cmd >> makepose name 2473 2035
+        #     """
+        #     name = inval[1]
+        #     idx1 = int(inval[2])
+        #     idx2 = int(inval[3])
+        #     makepose(name, idx1, idx2)
+
+        # elif cmd == 'test':
+        #     # runs repeated tests of a single grasp
+        #     """Example input:
+        #         $ cmd >> test name
+        #     """
+        #     name = inval[1]
+        #     while not rospy.is_shutdown():
+        #         if raw_input("Test again? [y/n] >> ") == 'n':
+        #             break
+
+        #         moveto('pre'+name)
+        #         rospy.sleep(2)
+        #         moveto(name)
+        #         rospy.sleep(2)
+        #         setclaw(True)
+        #         rospy.sleep(2)
+        #         moveto('post'+name)
+        #         rospy.sleep(4)
+        #         moveto(name)
+        #         rospy.sleep(2)
+        #         setclaw(False)
+        #         rospy.sleep(2)
+        #         moveto('pre'+name)
 
         else:
             print 'Bad command: '+inval[0]
