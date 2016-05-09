3a17759cadead97ceac362e2a4dd0136180edcff
Scout Heid <ssheid@berkeley.edu>
2016-05-09 09:28:26 -0700

    actually working sorting code -fixed issue where votesort wasnt sorting -revised min max sorting method

--- project4/src/grasper_plan/src/grasp_eval.py
+++ project4/src/grasper_plan/src/grasp_eval.py
@@ -20,8 +20,24 @@ presort = []
 vdistsort = []
 hdistsort = []
 cogdistsort = []
+apartsort = []
 
-def combine_vote(hsort,vsort,cogsort,presort):
+def topnum(grasps,maxmin,key,num):
+    topnum = []
+    clonegrasps = grasps
+    if maxmin == 'x':
+        for i in range(0,num):
+            topnum.append(max(grasps,key=lambda x:x[0][key]))
+            clonegrasps.remove(topnum[i])
+
+    if maxmin == 'n':
+        for i in range(0,num):
+            topnum.append(min(grasps,key=lambda x:x[0][key]))
+            clonegrasps.remove(topnum[i])
+
+    return topnum
+
+def combine_vote(hsort,vsort,cogsort,asort,presort):
     #1st heursitic is combination of dist between c1 and c2 of each grasp
     #2nd heursitic is z distance between them
     #3rd heuristic is how close it is cog
@@ -32,6 +48,8 @@ def combine_vote(hsort,vsort,cogsort,presort):
         hsort[k][0] = counter
         vsort[k][0] = counter
         cogsort[k][0] = counter
+        asort[k][0] = counter
+
         counter += 1
 
     # for graspsh in hsort:
@@ -49,21 +67,29 @@ def combine_vote(hsort,vsort,cogsort,presort):
     #     counter = counter + 1
 
     for item in rank:
+        item = item[1:2]
         for graspsh in hsort:
-            if graspsh == item:
+            graspsh = graspsh[1:2]
+            if np.array_equal(graspsh,item):
                 item[0][0] = graspsh[0] 
         for graspsv in vsort:
-            if graspsv == item:
+            graspsv = graspsv[1:2]
+            if np.array_equal(graspsv,item):
                 item[0][1] = graspsv[0] 
         for graspsc in cogsort:         
-            if graspsc == item:
+            graspsc = graspsc[1:2]
+            if np.array_equal(graspsc,item):
                 item[0][2] = graspsc[0]
+        for graspsa in asort: 
+            graspsa = graspsa[1:2]
+            if np.array_equal(graspsa,item):
+                item[0][3] = graspsa[0]
+
 
     for item in rank:
-        item[0] = item[0][0] + item[0][1] + item[0][2]
+        item[0] = item[0][0] + 2*item[0][1] + 2*item[0][2] + 2*item[0][3]
     
-    rank.sort(key=lambda x: x[0])
-    return rank
+    return rank.sort(key=lambda x: x[0])
 
 def combine_minmax(grasps):
     #1st heursitic is combination of dist between c1 and c2 of each grasp
@@ -71,12 +97,8 @@ def combine_minmax(grasps):
     #3rd heuristic is how close it is cog
     topfive = []
 
-    for i in range(0,5):
-        # topfive.append(max(grasps, key=lambda x: x[0][1]))
-        topfive.append(max(max(grasps,key=lambda x:x[0][1]), min(grasps,key=lambda x:x[0][2]),key=lambda x: x[0][0]))
-        # topfive.append(max(max(grasps,key=lambda x:x[0][0]),key=lambda x: x[0][1]))
-        grasps.remove(topfive[i])
-
+    # change this later to be a function of the size of the input grasps
+    topfive = topnum(topnum(topnum(topnum(grasps,'x',1,20),'n',3,15),'n',2,10),'x',0,5)
     return topfive
 
 def grasp_eval(grasp, cog=0.075):
@@ -92,8 +114,7 @@ def grasp_eval(grasp, cog=0.075):
             zg1 = grasp[i][0][11]
             zg2 = grasp[j][0][11]
             if np.abs(zg1 - zg2) > 0.05 and np.abs(zg1 - zg2)<.25:
-                presort.append([[0,0,0],grasp[i],grasp[j]])
-    print presort
+                presort.append([[0,0,0,0],grasp[i],grasp[j]])
 
     #rank by horizontal distance
     for grasps in presort:
@@ -102,8 +123,8 @@ def grasp_eval(grasp, cog=0.075):
   
     #add distance apart vertical
     for grasps in presort:
-        zg1 = grasp[1][0][11]
-        zg2 = grasp[2][0][11]
+        zg1 = grasps[1][0][11]
+        zg2 = grasps[2][0][11]
         vdistsort.append([abs(zg1-zg2),grasps[1],grasps[2]])
         grasps[0][1] = abs(zg1-zg2)
 
@@ -114,13 +135,23 @@ def grasp_eval(grasp, cog=0.075):
         cogdistsort.append([abs(zg1-cog)+abs(zg2-cog),grasps[1],grasps[2]])
         grasps[0][2] = abs(zg1-cog)+abs(zg2-cog)
 
+    #add same saide?
+    for grasps in presort:
+        zvect1 = grasps[1][0][8:10]
+        zvect1n = zvect1/np.linalg.norm(zvect1)
+        zvect2 = grasps[2][0][8:10]
+        zvect2n = zvect2/np.linalg.norm(zvect2)
+        apartsort.append([np.dot(zvect1n,zvect2n),grasps[1],grasps[2]])
+        grasps[0][3] = np.dot(zvect1n,zvect2n)
+
 
     hdistsort.sort(key=lambda x: x[0])
     vdistsort.sort(key=lambda x: x[0])
     cogdistsort.sort(key=lambda x: x[0])
+    apartsort.sort(key=lambda x: x[0])
 
     sortedminmax = combine_minmax(presort)
-    sortedvote = combine_vote(hdistsort,vdistsort,cogdistsort,presort)
+    sortedvote = combine_vote(hdistsort,vdistsort,cogdistsort,apartsort,presort)
 
     return sortedminmax, sortedvote
 
@@ -137,7 +168,6 @@ if __name__ == '__main__':
         grasps.append(tuple([eval(e) for e in row.split(';')]))
     in_f.close()
 
-    print grasps
     sortedgrasps, sortedvotegrasps = grasp_eval(grasps)
 
     out_f = open(SORTED_DATA_FILENAME1, 'w')
